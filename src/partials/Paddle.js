b50d9c3eb16499e2ad951915c75ce831ac40b2e1
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-02 16:49:00 -0700

    got paddles to start working and max and min height

--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -2,7 +2,7 @@ import { SVG_NS } from '../settings';
 
 export default class Paddle {
 
-  constructor(boardHeight, width, height, x, y) {
+  constructor(boardHeight, width, height, x, y, up, down) {
     this.boardHeight = boardHeight;
     this.width = width;
     this.height = height;
@@ -10,6 +10,28 @@ export default class Paddle {
     this.y = y;
     this.speed = 10;
     this.score = 0;
+
+    document.addEventListener("keydown", event => {
+      switch (event.key) {
+        case up:
+          this.up();
+          break;
+        case down:
+          this.down();
+          break;
+      }
+    });
+
+  } // end of constructor 
+
+  up() {
+  this.y = Math.max (0, this.y - this.speed);
+    
+  }
+
+  down() {
+  this.y = Math.min (200, this.y + this.speed);
+
   }
 
   render(svg) {
